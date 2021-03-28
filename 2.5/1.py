d16e56199af4ebf3ebd4eae545abdb23054f94c0
PatYoung <patyoungxyz@outlook.com>
2021-03-28 21:46:24 +0800

    update

--- 2.5/1.py
+++ 2.5/1.py
@@ -1,17 +1,16 @@
 import numpy as np 
 
-m = 1
-n = 401
+m = 0
+n = 4000
 a = [1,2,3,4]
 print(a)
 
-for i in range(1,n):
+for i in range(0,n):
     m = m + 1
-    a = [1,2,3,4]
     b = np.random.choice(a, size = None, replace = True, p = None)
     #print(b)
     bc = 0
-    ac = a
+    ac = a[:]
     bc = np.random.choice(ac, size = None, replace = True, p = None)
     #print(i,b,a)
     #print(ac,a)
@@ -23,4 +22,4 @@ for i in range(1,n):
         #print (bc)
 
 print(m)
-print(m/(n-1.0))
+print(m/(n))
